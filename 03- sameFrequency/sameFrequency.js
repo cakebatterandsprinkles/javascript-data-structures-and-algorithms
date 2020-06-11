96638fefec19e2cbe131e8fc5deb1f4ec3d9c493
yagmurcetintas <yagmurcetin@gmail.com>
2020-06-11 16:12:06 -0400

    deleted comments

--- 03- sameFrequency/sameFrequency.js
+++ 03- sameFrequency/sameFrequency.js
@@ -1,5 +1,4 @@
 function isSameFrequency(num1, num2) {
-  // Both numbers should be positive integers (no floating points/negative numbers/strings/arrays etc.)
   if (
     typeof num1 !== "number" ||
     typeof num2 !== "number" ||
@@ -11,15 +10,11 @@ function isSameFrequency(num1, num2) {
     return false;
   }
 
-  // Convert numbers to string to make them iterable
   let stringifiedNum1 = num1.toString();
   let stringifiedNum2 = num2.toString();
-
-  // Create a frequencyCounter object for each num
   let frequencyCounter1 = createFrequencyCounter(stringifiedNum1);
   let frequencyCounter2 = createFrequencyCounter(stringifiedNum2);
-  console.log(frequencyCounter1, frequencyCounter2);
-  // Check if they have the same amount of keys
+
   if (
     Object.keys(frequencyCounter1).length !==
     Object.keys(frequencyCounter2).length
@@ -27,7 +22,6 @@ function isSameFrequency(num1, num2) {
     return false;
   }
 
-  // Check if the values of those keys are the same
   for (let key in frequencyCounter1) {
     if (frequencyCounter1[key] !== frequencyCounter2[key]) {
       return false;
